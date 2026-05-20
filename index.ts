a658888c5a020298a5adc741ce40c0534902ecf8
ravshansbox <ravshansbox@gmail.com>
2026-05-20 15:36:38 +0300

    Update to latest pi packages

--- index.ts
+++ index.ts
@@ -16,8 +16,8 @@
  *   esc           - Close
  */
 
-import type { ExtensionAPI } from "@mariozechner/pi-coding-agent";
-import { visibleWidth, matchesKey, Key } from "@mariozechner/pi-tui";
+import type { ExtensionAPI } from "@earendil-works/pi-coding-agent";
+import { visibleWidth, matchesKey, Key } from "@earendil-works/pi-tui";
 import * as fs from "node:fs";
 import * as path from "node:path";
 import * as os from "node:os";
